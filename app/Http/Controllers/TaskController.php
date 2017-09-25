a42aba15b7e58c7fee578c0143b808ee55415687
introqt <introtheintro@gmail.com>
2017-09-25 20:42:44 +0300

    bla-bla

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,28 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
-class TaskController extends Controller
-{
+class TaskController extends Controller {
+
     /**
-   * Создание нового экземпляра контроллера.
-   *
-   * @return void
-   */
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
-  /**
- * Отображение списка всех задач пользователя.
- *
- * @param  Request  $request
- * @return Response
- */
-public function index(Request $request)
-{
-  return view('tasks.index');
-}
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Отображение списка всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+        return view('tasks.index');
+    }
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Создание задачи...
+    }
+
 }
